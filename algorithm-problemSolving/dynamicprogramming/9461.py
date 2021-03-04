80d4819d1d52f925b9287f529b39c0834b935c38
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-03-04 11:05:35 +0900

    Create 9461.py

---
+++ algorithm-problemSolving/dynamicprogramming/9461.py
@@ -0,0 +1,14 @@
+# BOJ 파도한 수열 9461번
+for tc in range(int(input())):
+  n = int(input())
+  
+  d = [0] * 101
+  d[1] = 1
+  d[2] = 1
+  d[3] = 1
+  
+  for i in range(4, n+1):
+    d[i] = d[i-3] + d[i-2]
+  
+  
+  print(d[n])
