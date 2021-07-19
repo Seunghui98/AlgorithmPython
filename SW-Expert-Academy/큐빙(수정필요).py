c6eff91d3b17c883d846f6ab301216a58dedc32b
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-07-19 10:58:08 +0900

    Update 큐빙(수정필요).py

--- SW-Expert-Academy/큐빙(수정필요).py
+++ SW-Expert-Academy/큐빙(수정필요).py
@@ -1,139 +1,102 @@
+# 아주 어려운 구현
+# 풀이 참조(https://crackerjacks.tistory.com/19)
+# 면-인덱스
+# 앞면(R-0)-아랫면(Y-1)-윗면(W-2)-왼쪽면(G-3)-오른쪽면(B-4)-뒷면(O-5)
 import sys
-
 input = sys.stdin.readline
-u_list = [['w', 'w', 'w'] for i in range(3)]
-d_list = [['y', 'y', 'y'] for i in range(3)]
-l_list = [['g', 'g', 'g'] for i in range(3)]
-r_list = [['b', 'b', 'b'] for i in range(3)]
-f_list = [['r', 'r', 'r'] for i in range(3)]
-b_list = [['o', 'o', 'o'] for i in range(3)]
-
-cube_list = []
-cube_list.append(u_list)
-cube_list.append(d_list)
-cube_list.append(l_list)
-cube_list.append(r_list)
-cube_list.append(f_list)
-cube_list.append(b_list)
-
-
-def rotation(what, dir):
-    if what == 'U' or what == 'D':
-        temp = [['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x']]
-        if what == 'U':
-            row = 0
-            if dir == '+':
-                # 뒤 -> 오 -> 앞 -> 왼 (시계)
-                index = [5, 3, 4, 2]
-            else:
-                # 뒤 -> 왼 -> 앞 -> 오 (반시계)
-                index = [5, 2, 4, 3]
-        else:
-            row = 2
-            if dir == '+':
-                # 뒤 -> 왼 -> 앞 -> 오 (반시계)
-                index = [5, 2, 4, 3]
-            else:
-                # 뒤 -> 오 -> 앞 -> 왼 (시계)
-                index = [5, 3, 4, 2]
-        for idx in range(len(index)):
-            for i in range(3):
-                if idx <= 2:
-                    temp[idx+ 1][i] = cube_list[index[idx]][row][i]
-                else:
-                    temp[0][i] = cube_list[index[idx]][row][i]
-        for idx in range(len(index)):
-            for i in range(3):
-                cube_list[index[idx]][row][i] = temp[idx][i]
-
-    elif what == 'F' or what == 'B':
-        temp = [['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x']]
-        if what == 'F':
-            row = 2
-            col = row - 2
-            if dir == '+':
-                # 윗 -> 오 -> 아 -> 왼 (시계)
-                index = [0, 3, 1, 2]
-            else:
-                # 윗 -> 왼 -> 아 -> 오 (반시계)
-                index = [0, 2, 1, 3]
-        else:
-            row = 0
-            col = row
-            if dir == '+':
-                print('음')
-                # 윗 -> 왼 -> 아 -> 오 (반시계)
-                index = [0, 2, 1, 3]
-            else:
-                # 윗 -> 오 -> 아 -> 왼 (시계)
-                index = [0, 3, 1, 2]
-
-        for idx in range(len(index)):
-            for i in range(3):
-                if idx == 0:
-                    temp[idx + 1][i] = cube_list[index[idx]][row][i]
-                elif idx == 1:
-                    temp[idx + 1][i] = cube_list[index[idx]][i][col]
-                elif idx == 2:
-                    temp[idx + 1][i] = cube_list[index[idx]][row][i]
-                else:
-                    temp[0][i] = cube_list[index[idx]][i][col+2]
-
-        for idx in range(len(index)):
-            for i in range(3):
-                if idx % 2 == 0:
-                    cube_list[index[idx]][row][i] = temp[idx][i]
-                else:
-                    if idx == 1:
-                        cube_list[index[idx]][i][2 - row] = temp[idx][i]
-                    else:
-                        cube_list[index[idx]][i][row] = temp[idx][i]
-
-
-    else:
-        temp = [['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x']]
-        if what == 'L':
-            col = 0
-            if dir == '+':
-                # 윗 -> 앞 -> 아 -> 뒤 (시계)
-                index = [0, 4, 1, 5]
-            else:
-                # 윗 -> 뒤 -> 아 -> 앞 (반시계)
-                index = [0, 5, 1, 4]
-        else:
-            col = 2
-            if dir == '+':
-                # 윗 -> 뒤 -> 아 -> 앞 (반시계)
-                index = [0, 5, 1, 4]
-            else:
-                # 윗 -> 앞 -> 아 -> 뒤 (시계)
-                index = [0, 4, 1, 5]
-
-        for idx in range(len(index)):
-            for i in range(3):
-                if idx <= 2:
-                    temp[idx + 1][i] = cube_list[index[idx]][i][col]
-                else:
-                    temp[0][i] = cube_list[index[idx]][i][col]
-        for idx in range(len(index)):
-            for i in range(3):
-                cube_list[index[idx]][i][col] = temp[idx][i]
-
-for tc in range(int(input())):
-    print(tc)
-    n = int(input())
-    order = list(map(str, input().split()))
-    for i in range(n):
-        rotation(order[i][0], order[i][1])
-        for i in range(3):
-            for j in range(3):
-                print(cube_list[0][i][j], end='')
-            print()
-        print()
+def printResult(cube):
     for i in range(3):
         for j in range(3):
-            print(cube_list[0][i][j], end='')
+            print(cube[2][i][j], end='')
         print()
 
 
+def moveDimension(cube, index):
+    for _ in range(2):
+        temp = cube[index][0][0]
+        cube[index][0][0] = cube[index][1][0]
+        cube[index][1][0] = cube[index][2][0]
+        cube[index][2][0] = cube[index][2][1]
+        cube[index][2][1] = cube[index][2][2]
+        cube[index][2][2] = cube[index][1][2]
+        cube[index][1][2] = cube[index][0][2]
+        cube[index][0][2] = cube[index][0][1]
+        cube[index][0][1] = temp
 
+
+
+
+def move(cube, direction):
+    if direction == 'U':
+        temp = cube[0][0]
+        cube[0][0] = cube[4][0]
+        cube[4][0] = cube[5][0]
+        cube[5][0] = cube[3][0]
+        cube[3][0] = temp
+        moveDimension(cube, 2)
+    elif direction == 'D':
+        temp = cube[0][2]
+        cube[0][2] = cube[3][2]
+        cube[3][2] = cube[5][2]
+        cube[5][2] = cube[4][2]
+        cube[4][2] = temp
+        moveDimension(cube, 1)
+
+    elif direction == 'F':
+        temp = cube[2][2]
+        cube[2][2] = [cube[3][2][2], cube[3][1][2], cube[3][0][2]]
+        cube[3][0][2], cube[3][1][2], cube[3][2][2] = cube[1][0]
+        cube[1][0] = [cube[4][2][0], cube[4][1][0], cube[4][0][0]]
+        cube[4][0][0], cube[4][1][0], cube[4][2][0] = temp
+        moveDimension(cube, 0)
+
+    elif direction == 'B':
+        temp = cube[2][0]
+        cube[2][0] = [cube[4][0][2], cube[4][1][2], cube[4][2][2]]
+        cube[4][2][2], cube[4][1][2], cube[4][0][2] = cube[1][2]
+        cube[1][2] = [cube[3][0][0], cube[3][1][0], cube[3][2][0]]
+        cube[3][2][0], cube[3][1][0], cube[3][0][0] = temp
+        moveDimension(cube, 5)
+
+    elif direction == 'L':
+        temp = [cube[0][0][0], cube[0][1][0], cube[0][2][0]]
+        cube[0][0][0], cube[0][1][0], cube[0][2][0] = cube[2][0][0], cube[2][1][0], cube[2][2][0]
+        cube[2][0][0], cube[2][1][0], cube[2][2][0] = cube[5][2][2], cube[5][1][2], cube[5][0][2]
+        cube[5][0][2], cube[5][1][2], cube[5][2][2] = cube[1][2][0], cube[1][1][0], cube[1][0][0]
+        cube[1][0][0], cube[1][1][0], cube[1][2][0] = temp
+        moveDimension(cube, 3)
+
+
+    elif direction == 'R':
+        temp = [cube[0][0][2], cube[0][1][2], cube[0][2][2]]
+        cube[0][0][2], cube[0][1][2], cube[0][2][2] = cube[1][0][2], cube[1][1][2], cube[1][2][2]
+        cube[1][0][2], cube[1][1][2], cube[1][2][2] = cube[5][2][0], cube[5][1][0], cube[5][0][0]
+        cube[5][0][0], cube[5][1][0], cube[5][2][0] = cube[2][2][2], cube[2][1][2], cube[2][0][2]
+        cube[2][0][2], cube[2][1][2], cube[2][2][2] = temp
+        moveDimension(cube, 4)
+
+
+def go(cube, comm):
+    direction, count = comm
+    count = 1 if count == '+' else 3
+    for _ in range(count): move(cube, direction)
+
+tc = int(input())
+
+for i in range(tc):
+    n = int(input())
+    comm = list(map(str, input().split()))
+    cube = [[] for _ in range(6)]
+    for _ in range(3):
+        cube[0].append(['r', 'r', 'r'])
+        cube[1].append(['y', 'y', 'y'])
+        cube[2].append(['w', 'w', 'w'])
+        cube[3].append(['g', 'g', 'g'])
+        cube[4].append(['b', 'b', 'b'])
+        cube[5].append(['o', 'o', 'o'])
+
+    for j in range(n):
+        go(cube, comm[j])
+
+
+    printResult(cube)
