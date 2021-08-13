c9d3f7ade4ed2297607b3211f86a79886df17e67
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-08-13 15:50:53 +0900

    Create 11659(투포인터).py

---
+++ BOJ-Solving/11659(투포인터).py
@@ -0,0 +1,18 @@
+# 구간 합 구하기 4 - BOJ 11659
+# 투포인터 - 구간 합 구하기
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+for _ in range(m):
+    left, right = map(int, input().split())
+    print(prefix_sum[right] - prefix_sum[left-1])
