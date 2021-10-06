3b9e5aa62bcfb4f5193f44fe59df9201cba67ca6
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-10-06 12:52:11 +0900

    Update 2636.py

--- BOJ-Solving/bfs&dfs/2636.py
+++ BOJ-Solving/bfs&dfs/2636.py
@@ -1,64 +1,53 @@
 import sys
 from collections import deque
 
-# 위 - 오 - 아래 - 왼 순서
-dr = [1, 0, -1, 0]
-dc = [0, 1, 0, -1]
+input = sys.stdin.readline
 
-H, W = map(int, sys.stdin.readline().split(" "))
-field = []
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for _ in range(H):
-    field.append(list(map(int, sys.stdin.readline().split(" "))))
-
-left = 0
-for i in range(H):
-    for j in range(W):
-        if field[i][j] == 1:
-            left += 1
-
-def bfs(left):
-    while queue:
-        now = queue.popleft()
-
+def bfs(a, b):
+    visited = [[False]*m for _ in range(n)]
+    q = deque()
+    q.append((a, b))
+    visited[a][b] = True
+    while q:
+        x, y = q.popleft()
         for i in range(4):
-            ndr = now[0] + dr[i]
-            ndc = now[1] + dc[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if visited[nx][ny] == False:
+                    if graph[nx][ny] == 1:
+                        graph[nx][ny] = 0
+                        visited[nx][ny] = True
+                        continue
+                    if graph[nx][ny] == 0:
+                        visited[nx][ny] = True
+                        q.append((nx, ny))
 
-            if 0 <= ndr < H and 0 <= ndc < W and visited[ndr][ndc] == 0:
-                visited[ndr][ndc] = 1
-
-                # 치즈 가장자리를 만나면
-                if field[ndr][ndc] == 1:
-                    field[ndr][ndc] = 2
-                    left -= 1
-                else:
-                    queue.append([ndr, ndc])
-
-    return left
+def check():
+    one_count = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                one_count += 1
+    return one_count
 
 
-def delete():
-    for i in range(H):
-        for j in range(W):
-            if field[i][j] == 2:
-                field[i][j] = 0
+time = 0
+last_cheese = 0
 
-queue = deque()
+while True:
+    cheese = check()
+    if cheese > 0:
+        bfs(0, 0)
+        time += 1
+        last_cheese = cheese
+    elif cheese == 0:
+        break
 
-count = 0
-temp = left
-while left != 0:
-    visited = [[0 for _ in range(W)] for _ in range(H)]
-    queue.append([0, 0])
-    visited[0][0] = 1
-    left = bfs(left)
-
-    if left != 0:
-        temp = left
-
-    count += 1
-    delete()
-
-print(count)
-print(temp)
+print(time)
+print(last_cheese)
