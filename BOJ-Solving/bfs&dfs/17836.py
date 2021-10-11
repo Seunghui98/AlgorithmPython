772540515a6f94e64f836e26568e500f9b1eb77e
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-10-11 22:56:01 +0900

    Update 17836.py

--- BOJ-Solving/bfs&dfs/17836.py
+++ BOJ-Solving/bfs&dfs/17836.py
@@ -1,30 +1,46 @@
+import sys
 from collections import deque
-n,m,T=map(int,input().split())
-maps=[list(map(int, input().split())) for _ in range(n)]
-visit=[[0 for _ in range(m)] for _ in range(n)]
-q=deque()
-q.append((0,0,0))
-dx,dy=(-1,1,0,0),(0,0,-1,1)
-result=float('inf')
-while q:
-    x,y,t=q.popleft()
-    if x==n-1 and y==m-1:
-        result=min(result,t)
-        break
-    if t+1>T : break
-    for dir in range(4):
-        nx,ny=x+dx[dir],y+dy[dir]
-        if 0<=nx<n and 0<=ny<m and visit[nx][ny]==0:
-            if maps[nx][ny]==1: continue
-            elif maps[nx][ny]==0:
-                visit[nx][ny]=True
-                q.append((nx,ny,t+1))
-            else:
-                visit[nx][ny]=True
-                tmp=t+1+abs(nx-(n-1))+abs(ny-(m-1))
-                if tmp<=T:
-                    result=tmp
-if result>T:
-    print("Fail")
+
+input = sys.stdin.readline
+
+
+n, m, t = map(int, input().split())
+visited = [[10001] * m for _ in range(n)]
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs(a, b):
+    q = deque()
+    q.append((a, b, 0))
+    visited[a][b] = 0
+
+    while q:
+        x, y, w = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if w == 1:
+                    if visited[nx][ny] >  visited[x][y] + 1:
+                        visited[nx][ny] = visited[x][y] + 1
+                        q.append((nx, ny, w))
+                else:
+                    if graph[nx][ny] == 1:
+                        continue
+                    elif graph[nx][ny] == 2:
+                        if visited[nx][ny] > visited[x][y] + 1:
+                            visited[nx][ny] = visited[x][y] + 1
+                            q.append((nx, ny, 1))
+                    else:
+                        if visited[nx][ny] > visited[x][y] + 1:
+                            visited[nx][ny] = visited[x][y] + 1
+                            q.append((nx, ny, w))
+
+
+bfs(0, 0)
+
+if visited[n-1][m-1] > t:
+    print('Fail')
 else:
-    print(result)​
+    print(visited[n-1][m-1])
