7fee180e2b6dfb444462cd7a891c0494dc0dfd0c
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-10-13 20:53:57 +0900

    Update and rename 순열조합.py to 순열조합(재귀).py

--- for_Coding_Test/순열조합(재귀).py
+++ for_Coding_Test/순열조합(재귀).py
@@ -27,3 +27,32 @@ def permutation(lst, n):
             for p in permutation(temp, n-1):
                 result.append([lst[i]]+p)
     return result
+
+# 중복순열
+def dul_permutation(lst, n):
+    result = []
+    if n > len(lst):
+        return result
+    if n == 1:
+        for i in lst:
+            result.append([i])
+    elif n > 1:
+        for i in range(len(lst)):
+            temp = [i for i in lst]
+            for p in permutation(temp, n-1):
+                result.append([lst[i]]+p)
+    return result
+
+# 중복조합
+def dul_combination(lst, n):
+    result = []
+    if n > len(lst):
+        return result
+    if n == 1:
+        for i in lst:
+            result.append([i])
+    elif n > 1:
+        for i in range(len(lst)-n+1):
+            for temp in combination(lst[i:], n-1):
+                result.append([lst[i]]+temp)
+    return result
