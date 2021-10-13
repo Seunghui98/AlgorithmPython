5b61b7fe8de85d1e2c059dab4b6a3557b8325679
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-10-13 21:07:01 +0900

    Create 순열조합(dfs).py

---
+++ for_Coding_Test/순열조합(dfs).py
@@ -0,0 +1,92 @@
+# 조합
+def dfs_combination(lst, n):
+    ret = []
+    idx = [i for i in range(len(lst))]
+
+    stack = []
+    for i in idx[:len(lst) - n + 1]:
+        stack.append([i])
+
+    while len(stack) != 0:
+        cur = stack.pop()
+
+        for i in range(cur[-1] + 1, len(lst) - n + 1 + len(cur)):
+            temp = cur + [i]
+            if len(temp) == n:
+                element = []
+                for i in temp:
+                    element.append(lst[i])
+                ret.append(element)
+            else:
+                stack.append(temp)
+    return ret
+
+# 중복 조합
+def dfs_dul_combination(lst, n):
+    ret = []
+    idx = [i for i in range(len(lst))]
+
+    stack = []
+    for i in idx[:len(lst) - n + 1]:
+        stack.append([i])
+
+    while len(stack) != 0:
+        cur = stack.pop()
+
+        for i in range(cur[-1], len(lst) - n + 1 + len(cur)):
+            temp = cur + [i]
+            if len(temp) == n:
+                element = []
+                for i in temp:
+                    element.append(lst[i])
+                ret.append(element)
+            else:
+                stack.append(temp)
+    return ret
+
+# 순열
+def dfs_permutation(lst, n):
+    ret = []
+    idx = [i for i in range(len(lst))]
+
+    stack = []
+    for i in idx:
+        stack.append([i])
+
+    while len(stack) != 0:
+        cur = stack.pop()
+
+        for i in idx:
+            if i not in cur:
+                temp = cur + [i]
+                if len(temp) == n:
+                    element = []
+                    for i in temp:
+                        element.append(lst[i])
+                    ret.append(element)
+                else:
+                    stack.append(temp)
+    return ret
+
+# 중복 순열
+def dfs_dul_permutation(lst, n):
+    ret = []
+    idx = [i for i in range(len(lst))]
+
+    stack = []
+    for i in idx:
+        stack.append([i])
+
+    while len(stack) != 0:
+        cur = stack.pop()
+
+        for i in idx:
+            temp = cur + [i]
+            if len(temp) == n:
+                element = []
+                for i in temp:
+                    element.append(lst[i])
+                ret.append(element)
+            else:
+                stack.append(temp)
+    return ret
