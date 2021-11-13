918fd2ae6be0a259567b62db29d37711602f55ea
Ellen <54658745+Seunghui98@users.noreply.github.com>
2021-11-13 20:07:23 +0900

    Update 2583(푸는중).py

--- BOJ-Solving/bfs&dfs/2583(푸는중).py
+++ BOJ-Solving/bfs&dfs/2583(푸는중).py
@@ -2,53 +2,37 @@
 from collections import deque
 
 n, m = map(int, input().split())
-graph = [input() for _ in range(n)]
+graph = [list(input().strip()) for _ in range(n)]
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(x, y, visited, cnt):
-    visited[x][y] = cnt
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if visited[nx][ny] == 0:
-                dfs(nx, ny, visited, cnt)
 
-def bfs(x, y, visited, cnt):
-    v = [[-1]*m for _ in range(n)]
-    for i in range(n):
-        print(v[i])
-    v[x][y] = 0
+def bfs(x, y):
     q = deque()
-    q.append((x, y))
+    q.append([x, y])
+    max_n = 0
     while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if visited[nx][ny] == cnt and v[nx][ny] == -1:
-                    v[nx][ny] = v[x][y] + 1
-                    q.append((nx, ny))
-    mm = 0
-    for i in range(n):
-        mm = max(mm, max(v[i]))
-        print(v[i])
+        a, b = q.popleft()
+        for k in range(4):
+            x = a + dx[k]
+            y = b + dy[k]
+            if 0 <= x < n and 0 <= y < m and visit[x][y] == 0 and graph[x][y] != "W":
+                visit[x][y] = 1
+                graph[x][y] = graph[a][b] + 1
+                max_n = max(max_n, graph[x][y])
+                q.append([x, y])
+    return max_n
 
-    print(mm)
-    print()
-    return mm
 
-answer = int(1e9)
-
-visited = [[0] * m for _ in range(n)]
-cnt = 1
+result = 0
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 'L' and visited[i][j] == 0:
-            dfs(i, j, visited, cnt)
-            answer = min(answer, bfs(i, j, visited, cnt))
-            cnt += 1
+        if graph[i][j] != "W":
+            visit = [[0]*m for _ in range(n)]
+            graph[i][j] = 0
+            visit[i][j] = 1
+            result = max(result, bfs(i, j))
 
-print(answer)
+
+print(result)
